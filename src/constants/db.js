0520de2c6338fb7da6cf6dabb6a54a716dd4b0f2
AkobirCoder <akobir0308@gmail.com>
2026-02-14 00:01:24 +0500

    src/constants/db.js was updated

--- src/constants/db.js
+++ src/constants/db.js
@@ -2,60 +2,88 @@
 export function getData() {
 	return [
 		{
-			title: 'JavaScript',
-			price: 17.99,
+			title: 'Foundation',
+			price: 12.5,
 			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2F3gf746AKRbWNjB8OCoEB&w=3840&q=75',
+				'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo00Gmjkh79cpx21MsQhvWPLIgeAHGwlaumbU8j&w=1920&q=75&dpl=dpl_9sBEKp6Vtq4rX75YeLkgkG6tPcnw',
 			id: 1,
 		},
 		{
-			title: 'ReactJS',
-			price: 15,
+			title: 'JavaScript',
+			price: 17.99,
 			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2F65rcPxsLT9ysJDisXF80&w=3840&q=75',
+				'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo01pCyzWVJWIAxq9EjKgNoL7zftvlDbOmru0H2&w=1920&q=75&dpl=dpl_9sBEKp6Vtq4rX75YeLkgkG6tPcnw',
 			id: 2,
-		},
-		{
-			title: 'VueJS',
-			price: 13.5,
-			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2F54vR0DStGoFuZBVkFwWQ&w=3840&q=75',
-			id: 3,
-		},
-		{
-			title: 'NodeJS',
-			price: 13.99,
-			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FmDcwhbguQpyM74jb18M5&w=3840&q=75',
-			id: 4,
-		},
-		{
-			title: 'NestJS',
-			price: 12.5,
-			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FxN7aKkLMQUuD72ikvC1H&w=3840&q=75',
-			id: 5,
-		},
-		{
-			title: 'NextJS',
-			price: 12.99,
-			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FPe94ubZPSCHgXDIPBxTr&w=3840&q=75',
-			id: 6,
 		},
 		{
 			title: 'TypeScript',
 			price: 22.99,
 			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FfZ3plnBeRQ2hJtZNbhNE&w=3840&q=75',
+				'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0E5lF6amAU8kioLXqN3YRt4exAMZ2yFscwGIl&w=1920&q=75&dpl=dpl_9sBEKp6Vtq4rX75YeLkgkG6tPcnw',
+			id: 3,
+		},
+		{
+			title: 'React.js',
+			price: 20.99,
+			Image:
+				'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0iUp6fYhocXUlvFEwuZWOT81zHSksDrGxN4f9&w=1920&q=75&dpl=dpl_9sBEKp6Vtq4rX75YeLkgkG6tPcnw',
+			id: 4,
+		},
+		{
+			title: 'Vue.js',
+			price: 13.5,
+			Image:
+				'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0pJtmiWIV0jo2XA16Ydy4rSfwLIiRxv98gOuk&w=1920&q=75&dpl=dpl_9sBEKp6Vtq4rX75YeLkgkG6tPcnw',
+			id: 5,
+		},
+		{
+			title: 'Angular',
+			price: 22.99,
+			Image:
+				'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fwww.sammi.ac%2F_next%2Fimage%3Furl%3Dhttps%253A%252F%252Fqk9g5hsrut.ufs.sh%252Ff%252Fxu965PmyvPo029yCLv51xPWtVAgLib0hZoQDvKECnGHuYdBj%26w%3D1920%26q%3D75%26dpl%3Ddpl_AyWjnwefx2RFAeEprCzJhU8VkTuz&w=1920&q=75&dpl=dpl_9sBEKp6Vtq4rX75YeLkgkG6tPcnw',
+			id: 6,
+		},
+		{
+			title: 'Next.js',
+			price: 25.5,
+			Image:
+				'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo041nrlbd9utCZemPzyoJSOTAbr18IvF0MYQ6H&w=1920&q=75&dpl=dpl_9sBEKp6Vtq4rX75YeLkgkG6tPcnw',
 			id: 7,
 		},
 		{
-			title: 'TensorflowJS',
+			title: 'Node.js',
 			price: 28.99,
 			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FtIMgibghQveiDeJeXDPI&w=3840&q=75',
+				'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0Dt6eWDpQTl9a4CLgOmnEFkfdiNbwx068Xc3o&w=1920&q=75&dpl=dpl_9sBEKp6Vtq4rX75YeLkgkG6tPcnw',
 			id: 8,
 		},
+		{
+			title: 'Nest.js',
+			price: 25.5,
+			Image:
+				'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0WO04xMuv70rfVnCNDiebGQXS2lh4oIUJ8tdw&w=1920&q=75&dpl=dpl_9sBEKp6Vtq4rX75YeLkgkG6tPcnw',
+			id: 9,
+		},
+        {
+            title: 'React Native',
+            price: 20.99,
+            Image:
+                'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0UVXRCCHlS4Qy1xdrtYVH8eZKj5upDiUCm7hs&w=1920&q=75&dpl=dpl_9sBEKp6Vtq4rX75YeLkgkG6tPcnw',
+            id: 10,
+        },
+        {
+            title: 'Telegram bot',
+            price: 17.99,
+            Image:
+                'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0FM6vDAPKdjJrMesifW4Gtho7ID1zXTlQ025w&w=1920&q=75&dpl=dpl_9sBEKp6Vtq4rX75YeLkgkG6tPcnw',
+            id: 11,
+        },
+        {
+            title: 'Webpack',
+            price: 22.5,
+            Image:
+                'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0z3TZofoOgTrMqidoG8sfm7k3ePCQ2VuYZtKa&w=1920&q=75&dpl=dpl_9sBEKp6Vtq4rX75YeLkgkG6tPcnw',
+            id: 12,
+        },
 	];
 }

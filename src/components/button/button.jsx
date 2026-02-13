0e30a3522bf4df8cf9d5fa3ca3b92b15c2e66446
AkobirCoder <akobir0308@gmail.com>
2026-02-14 01:04:39 +0500

    src/components/button/button.jsx was added

---
+++ src/components/button/button.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './button.css';
+
+const Button = ({type, title}) => {
+    return (
+        <button className={`btn ${
+            type === 'add' && 'add' ||
+            type === 'remove' && 'remove' ||
+            type === 'checkout' && 'checkout'
+        }`}>
+            {title}
+        </button>
+    );
+}
+
+export default Button;

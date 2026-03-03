bfb5aff52cbbf3d8691c2b6003ea3f8563a341f5
AkobirCoder <akobir0308@gmail.com>
2026-03-04 01:17:55 +0500

    src/App.jsx was updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { getData } from './constants/db';
 import { Card, Cart } from './components/index';
 
 const App = () => {
     const courses = getData();
+
+    const [cartItems, setCartItems] = useState([]);
+
+    const onAddItems = (item) => {
+        const existItem = cartItems.find((cartItem) => {
+            return cartItem.id === item.id;
+        });
+
+        console.log('EXIST_ITEM', existItem);
+
+        if (existItem) {
+            const newData = cartItems.map((cartItem) => {
+                return cartItem.id === item.id 
+                    ? {...existItem, quantity: existItem.quantity + 1}
+                    : cartItem;
+            });
+
+            console.log('ADD_QUANTITY_EXIST_ITEM', newData);
+
+            setCartItems(newData);
+        } else {
+            const newData = [...cartItems, {...item, quantity: 1}];
+
+            console.log('ADD_NEW_ITEM', newData);
+
+            setCartItems(newData);
+        }
+    }
+
+    const onRemoveItems = (item) => {
+        const existItem = cartItems.find((cartItem) => {
+            return cartItem.id === item.id;
+        });
+
+        console.log('existItem', existItem);
+
+        if (existItem.quantity === 1) {
+            const newData = cartItems.filter((cartItem) => {
+                return cartItem.id !== existItem.id;
+            });
+
+            console.log('DELETE_ITEM_QUANTITY_0', newData);
+
+            setCartItems(newData);
+        } else {
+            const newData = cartItems.map((cartItem) => {
+                return cartItem.id === existItem.id
+                    ? {...existItem, quantity: existItem.quantity - 1}
+                    : cartItem;
+            });
+
+            console.log('DELETE_1_ITEM_QUANTITY', newData);
+
+            setCartItems(newData);
+        }
+    }
 
     return (
         <>
@@ -17,6 +73,9 @@ const App = () => {
                             <Card
                                 key={course.id}
                                 {...course}
+                                course={course}
+                                onAddItems={onAddItems}
+                                onRemoveItems={onRemoveItems}
                             />
                         );
                     })

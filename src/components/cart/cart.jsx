52085e220ccc89d1a874afadeeec504d76a3946b
AkobirCoder <akobir0308@gmail.com>
2026-03-05 16:41:23 +0500

    src/components/cart/cart.jsx was updated

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import './cart.css';
 import { Button } from '../index';
+import { totalPrice } from '../../units/total-price';
 
-const Cart = () => {
+const Cart = ({cartItems}) => {
     return (
         <div className='cart__container'>
-            <p>Umumiy narx: $12.00</p>
-            <Button title={'Buyurtma'} type={'checkout'} />
+            <p>
+                Umumiy narx: {
+                    totalPrice(cartItems).toLocaleString("en-US", {
+                        style: "currency",
+                        currency: "USD"
+                    })
+                }
+            </p>
+            <Button 
+                title={`${cartItems.length === 0 ? 'Buyurtma berish' : "To'lov"}`} 
+                type={'checkout'} 
+                disable={cartItems.length === 0 ? true : false}
+            />
         </div>
     );
 }
